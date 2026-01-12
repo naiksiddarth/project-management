2b9f7da4e961a5843ac2a9343f53305c6331269d
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-01-12 20:34:40 +0530

    Add code to create accessToken refreshToken and temporaryToken

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -61,5 +61,39 @@ const userSchema = new Schema(
     }
 )
 
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    )
+}
+
+userSchema.methods.generateTemporaryToken = function (params) {
+    const unHashedToken = crypto.randomBytes(16)
+    const hasedToken = crypto.createHash("sha256").update(unHashedToken).digest("hex")
+    const tokenExpiry = Date.now() + (20*60*1000) // 20 mins 
+    return { unHashedToken, hasedToken, tokenExpiry }
+}
 
 export const User = mongoose.model("User", userSchema)
