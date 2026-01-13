b1c07aad8e4c5f540a864f708ea76e67388ea33f
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-01-13 23:47:33 +0530

    Register route in express app

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import express from "express"
 import cors from "cors"
 
 const app = express()
-
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use(
     cors({
         origin: process.env.CORS_ORIGIN,
@@ -12,7 +13,10 @@ app.use(
 )
 
 import healthCheckRouter from "./routes/healthcheck.routes.js"
+import authRouter from "./routes/auth.routes.js"
+
 app.use("/api/v1/healthcheck", healthCheckRouter)
+app.use("/api/v1/auth", authRouter)
 
 app.get("/", (req, res) => {
     res.send("Hello")
