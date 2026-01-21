abeda40ae1b7da07556bce15c1f20dfd6b7e2753
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-01-21 22:34:34 +0530

    Add cookie middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 app.use(
     cors({
         origin: process.env.CORS_ORIGIN,
