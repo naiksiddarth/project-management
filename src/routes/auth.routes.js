3427c2a03b258798f0620fb48450621acae2374e
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-01-21 22:40:52 +0530

    Add login route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { registerUser } from "../controllers/auth.controllers.js"
+import { registerUser, loginUser } from "../controllers/auth.controllers.js"
 import { validateRegister } from "../validators/auth.validators.js"
 import { validate } from "../middlewares/validator.middlewares.js"
 
 const router = Router();
 router.route("/register").post(validateRegister(), validate, registerUser)
+router.route('/login').post(loginUser)
 
 export default router;
