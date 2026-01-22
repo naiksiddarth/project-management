569b39feadd7f02b8078803e7105471187df5d9a
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-01-22 23:09:53 +0530

    Add code to verify access token

--- src/middlewares/validator.middlewares.js
+++ src/middlewares/validator.middlewares.js
@@ -1,5 +1,7 @@
 import { validationResult } from "express-validator"
 import { ApiError } from "../utils/api-error.js"
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.models.js"
 
 export const validate = function (req, res, next) {
     const errors = validationResult(req)
@@ -9,4 +11,21 @@ export const validate = function (req, res, next) {
         extractedErrors.push({[error.path]: error.msg})
     })
     throw new ApiError(422, "Recieved data is not valid", extractedErrors)
+}
+
+export const verifyJWT = async function (req, res, next) {
+    const accessToken = req.cookies.accessToken || req.headers.authorization?.split(" ")[1]
+    if(!accessToken) {
+        throw new ApiError(400, "Access Token not found in cookies or headers")
+    }
+    let user = undefined
+    try{
+        user = await jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+    } catch(err) {
+        throw new ApiError(400, "Error verifying access token")
+    }
+    const loggedInUser = await User.findById(user._id)
+
+    req.user = loggedInUser
+    return next()
 }
