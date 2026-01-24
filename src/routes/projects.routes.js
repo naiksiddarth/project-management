0a4c6347d0910738fbbe01d0e73bd20f5a180ac8
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-01-24 21:53:21 +0530

    Add route for get /api/v1/projects/

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import { createProject } from "../controllers/project.controllers.js"
+import { createProject, getProjects } from "../controllers/project.controllers.js"
 import { verifyJWT } from "../middlewares/validator.middlewares.js"
 
 const router = Router()
 
 router.route("/")
     .post( verifyJWT, createProject )
+    .get( verifyJWT, getProjects)
 
 export default router
